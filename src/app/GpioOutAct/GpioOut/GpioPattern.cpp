3396dafca58bd9eb8d226f5ea5085434db57085b
Sarah Berrier <47516313+SarahBerrier@users.noreply.github.com>
2022-05-06 18:37:41 -0700

    Initial embsys 310 files

---
+++ src/app/GpioOutAct/GpioOut/GpioPattern.cpp
@@ -0,0 +1,25 @@
+#include "GpioPattern.h"
+
+namespace APP {
+
+// Illustration only.
+/*
+GpioInterval testInterval = {1, 2};
+
+GpioPattern testPattern = {
+    2, {{800,500}, {0, 500}}
+};
+*/
+
+GpioPatternSet const TEST_GPIO_PATTERN_SET = {
+    2, 
+    {
+        // Assignment 2 - Add 2 LED patterns here.
+        // Pattern 0
+        // { ... },
+        // Pattern 1
+    	// { ... }
+    }
+};
+
+} // namespace APP

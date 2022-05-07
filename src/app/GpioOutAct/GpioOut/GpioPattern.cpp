ab98c203d9dcada1b10030369ca7e3f849db4c8b
Sarah Berrier <47516313+SarahBerrier@users.noreply.github.com>
2022-05-06 18:44:53 -0700

    HW2: Implement two LED test patterns

--- src/app/GpioOutAct/GpioOut/GpioPattern.cpp
+++ src/app/GpioOutAct/GpioOut/GpioPattern.cpp
@@ -12,13 +12,13 @@ GpioPattern testPattern = {
 */
 
 GpioPatternSet const TEST_GPIO_PATTERN_SET = {
-    2, 
+    2,
     {
         // Assignment 2 - Add 2 LED patterns here.
         // Pattern 0
-        // { ... },
-        // Pattern 1
-    	// { ... }
+       	{2, {{800,500}, {200, 500}}},
+    	// Pattern 1
+    	{4, {{0, 500}, {1000,500}, {0, 500}, {100,500}}}
     }
 };
 
